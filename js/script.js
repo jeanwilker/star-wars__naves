549a36aa4fb196646d9c2f680fc9a677a7f79fc4
Jean Wilker <jeanwilkerss@gmail.com>
2022-09-28 18:58:22 -0300

    Adiciona tab menu ao conteúdo

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function initTabNav() {
+  const tabMenu = document.querySelectorAll('.js-tab__menu li');
+  const tabContent = document.querySelectorAll('.js-tab__content section');
+
+  if(tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add('active');
+
+    function activeTab(index) {
+      tabContent.forEach((section => {
+        section.classList.remove('active');
+      }));
+      tabContent[index].classList.add('active');
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => {
+        activeTab(index);
+      })
+    })
+  }
+}
+initTabNav();
