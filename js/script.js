e981f3d1ff00197f540c28854f72d532cd00a1d5
Jean Wilker <jeanwilkerss@gmail.com>
2022-09-29 18:35:53 -0300

    Cria animação scroll.

--- js/script.js
+++ js/script.js
@@ -59,3 +59,28 @@ function initSmoothScroll() {
     link.addEventListener('click', scrollSection);
   });
 }initSmoothScroll();
+
+function initScrollAnimation() {
+  const sections = document.querySelectorAll('.js-scroll');
+
+  if(sections.length) {
+    const halfWindow = window.innerHeight * 0.6;
+
+  function animateScroll() {
+    sections.forEach((section) => {
+      const sectionTop = section.getBoundingClientRect().top;
+      const isSectionVisible = (sectionTop - halfWindow) < 0;
+
+      if(isSectionVisible) {
+        section.classList.add('active');
+      }else {
+        section.classList.remove('active');
+      }
+    });
+  }
+  animateScroll();
+
+  window.addEventListener('scroll', animateScroll);
+
+  }
+}initScrollAnimation();
