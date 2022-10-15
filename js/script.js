127ea0627be96e22251c5206ecab7ad3d2f04908
Jean Wilker <jeanwilkerss@gmail.com>
2022-10-14 23:26:46 -0300

    Cria modal de login.

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ import initScrollAnimation from './modules/scrollAnimation.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabNav.js';
 import initNavbarMobile from './modules/navbarMobile.js';
+import initModal from './modules/modal.js'
 
 initSmoothScroll();
 initScrollAnimation();
 initAccordion();
 initTabNav();
 initNavbarMobile();
+initModal();
